2642dbef35a942f8722bf7e264937e22952cdaf3
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-02-17 14:15:14 -0300

    Agregando repositorio

--- app/src/main/java/com/example/earthquakemonitor/MainViewModel.kt
+++ app/src/main/java/com/example/earthquakemonitor/MainViewModel.kt
@@ -13,46 +13,15 @@ class MainViewModel: ViewModel() {
     val eqList: LiveData<MutableList<Earthquake>>
       get() = _eqList
 
+    val repository = MainRepository()
+
     init {
        viewModelScope.launch {
-            _eqList.value = fetchEarthquakes()
+            _eqList.value = repository.fetchEarthquakes()
         }
     }
 
-   private suspend fun fetchEarthquakes(): MutableList<Earthquake> {
-      return withContext(Dispatchers.IO) {
-          val eqListString = service.getLastHourEarthquakes()
-          val eqList = parseEqResult(eqListString)
 
-          eqList
-      }
-    }
-
-    private fun parseEqResult(eqListString: String): MutableList<Earthquake> {
-      val eqJsonObject = JSONObject(eqListString)
-      val featuresJsonArray = eqJsonObject.getJSONArray("features") //Obtenemos el array features
-
-        val eqList = mutableListOf<Earthquake>()
-
-      for (i in 0 until featuresJsonArray.length()) {
-          val featuresJsonObject = featuresJsonArray[i] as JSONObject
-          val id = featuresJsonObject.getString("id")
-
-          val propertiesJsonObject = featuresJsonObject.getJSONObject("properties")
-          val magnitude = propertiesJsonObject.getDouble("mag")
-          val place = propertiesJsonObject.getString("place")
-          val time = propertiesJsonObject.getLong("time")
-
-          val geometryJsonObject = featuresJsonObject.getJSONObject("geometry")
-          val coordinatesJsonArray  = geometryJsonObject.getJSONArray("coordinates")
-          val longitude = coordinatesJsonArray.getDouble(0)
-          val latitude = coordinatesJsonArray.getDouble(1)
-
-          val earthquake = Earthquake(id, place, magnitude, time, longitude, latitude)
-          eqList.add(earthquake)
-      }
-        return eqList
-    }
 
 
 }
