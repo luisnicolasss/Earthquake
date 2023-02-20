2653ad4cebdfadfabe3d96664839943969826ca4
luisnicolasss <86482520+luisnicolasss@users.noreply.github.com>
2023-02-20 16:14:27 -0300

    Creando DAO y base de datos

---
+++ app/src/main/java/com/example/earthquakemonitor/database/EqDatabase.kt
@@ -0,0 +1,24 @@
+package com.example.earthquakemonitor.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.earthquakemonitor.Earthquake
+
+@Database(entities = [Earthquake::class], version = 1)
+abstract class EqDatabase: RoomDatabase() {
+    abstract val eqDao: EqDao
+}
+
+//Singleton: Variable que se instancia solo una vez y se puede usar en toda ocasión
+private lateinit var INSTANCE: EqDatabase
+
+fun getDatabase(context: Context): EqDatabase {
+    synchronized(EqDatabase::class.java) {
+        if(!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext, EqDatabase::class.java, "earthquake_db").build()
+        }
+        return INSTANCE
+    }
+}
